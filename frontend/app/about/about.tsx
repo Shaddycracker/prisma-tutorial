e6ee766052a922323cbc24bb4dbbc80287d7ddd8
SHADABALIGITHUB <shad46243@gmail.com>
2025-06-30 22:48:53 +0530

    removed next start react app

---
+++ frontend/app/about/about.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutUs: React.FC = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This project is a demonstration of using React Router with TypeScript.</p>
+      <p>We aim to provide a seamless navigation experience in our application.</p>
+    </div>
+  );
+}
+export default AboutUs;
